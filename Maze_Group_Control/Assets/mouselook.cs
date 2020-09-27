560c5ef0a292a35f39223c7e5a4d5daef5e7409a
Gerardo Herrera <gerardohe17@gmail.com>
2020-09-26 23:12:20 -0400

    camera test

--- Maze_Group_Control/Assets/mouselook.cs
+++ Maze_Group_Control/Assets/mouselook.cs
@@ -8,7 +8,7 @@ public class mouselook : MonoBehaviour
 
     public Transform playerBody;
 
-    float xRotation = 0f;
+    float xRotation = 90f;
     // Start is called before the first frame update
     void Start()
     {
