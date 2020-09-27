41a338ca2ddd878fa5bb54ddeda56090f5280b37
Gerardo Herrera <gerardohe17@gmail.com>
2020-09-26 21:37:16 -0400

    cam movement

--- Maze_Group_Control/Assets/WASD.cs
+++ Maze_Group_Control/Assets/WASD.cs
@@ -6,6 +6,7 @@ public class WASD : MonoBehaviour
 {
     // Start is called before the first frame update
     float speed = 0.02f;
+    
     void Start()
     {
         
@@ -15,12 +16,12 @@ public class WASD : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.W))
-            transform.position += Vector3.up * speed;
-        if (Input.GetKey(KeyCode.S))
             transform.position += Vector3.down * speed;
+        if (Input.GetKey(KeyCode.S))
+            transform.position += Vector3.up * speed;
         if (Input.GetKey(KeyCode.A))
-            transform.position += Vector3.right * speed;
-        if (Input.GetKey(KeyCode.D))
             transform.position += Vector3.left * speed;
+        if (Input.GetKey(KeyCode.D))
+            transform.position += Vector3.right * speed;
     }
 }
