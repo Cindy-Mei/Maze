560751d1edd8d933892bde59fe826ebaa9792bf1
Cindy Mei <cindy.mei1@mail.citytech.cuny.edu>
2020-09-28 19:33:21 -0400

    added on collider for keys

---
+++ Maze_Group_Control/Assets/collision_key.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collision_key : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    public GameObject uiObject;
+
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider playerCharacter){
+
+      uiObject.SetActive(true);
+      StartCoroutine("WaitForSec");
+
+    }
+
+    IEnumerator WaitForSec(){
+      yield return new WaitForSeconds(5);
+    }
+}
