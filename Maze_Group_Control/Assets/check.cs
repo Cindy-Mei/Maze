c582d3b5c2e961c798885ac843a15770ddda0920
Julian <Julian.frost101@gmail.com>
2020-09-28 20:49:19 -0400

    button position

---
+++ Maze_Group_Control/Assets/check.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class check : MonoBehaviour
+{
+
+    private string code = "0180";
+    public InputField Input;
+    public string answer;
+
+
+   public void checkcode()
+    {
+        answer = Input.text;
+
+        if(answer!=null)
+        {
+
+            if (answer == code)
+            {
+                //door code here
+
+            }
+
+            else
+            {
+                Debug.Log("wrong");
+
+            }
+                
+                
+                
+        }
+
+
+
+
+
+
+    }
+}
